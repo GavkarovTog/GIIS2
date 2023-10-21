bb84a75e0321372f4678cb65bcc4f3747d985ddb
GavkarovTog <124872359+GavkarovTog@users.noreply.github.com>
2023-10-21 17:37:56 +0300

    Ermit, Bezier, BSpline curves

--- graphical_editor.dart
+++ graphical_editor.dart
@@ -1,4 +1,8 @@
-List<Point> getLineDDAPoints(Offset start, Offset end) {
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:graphical_editor/utils/utils.dart';
+
+List<Point> getLineDDAPoints(Offset start, Offset end, {Color color=Colors.black}) {
   List<Point> points = [];
 
   if (start == end) {
@@ -13,14 +17,14 @@ List<Point> getLineDDAPoints(Offset start, Offset end) {
   double x = start.dx.round().toDouble();
   double y = start.dy.round().toDouble();
 
-  points.add(Point.vec2b(x.toInt(), y.toInt()));
+  points.add(Point.vec2(x.toInt(), y.toInt(), color));
 
   int i = 0;
-  while (i < length) {
+  while (i <= length) {
     x = x + dx;
     y = y + dy;
 
-    points.add(Point.vec2b(x.toInt(), y.toInt()));
+    points.add(Point.vec2(x.toInt(), y.toInt(), color));
     i++;
   }
 
@@ -36,6 +40,10 @@ List<Point> getLineBresenhamPoints(Offset start, Offset end) {
 
   List<Point> points = [Point.vec2b(x, y)];
   bool xIsLeading = dy.abs() < dx.abs();
+
+  if (dx == 0 || dy == 0) {
+    return getLineDDAPoints(start, end);
+  }
 
   int octant = 1;
   if (dx < 0 && dy > 0) {
@@ -235,7 +243,7 @@ List<Point> getCircleBresenhamPoints(Offset start, Offset end) {
   List<Point> points = [];
 
   int radius =
-      sqrt(pow(end.dx - start.dx, 2) + pow(end.dy - start.dy, 2)).toInt();
+  sqrt(pow(end.dx - start.dx, 2) + pow(end.dy - start.dy, 2)).toInt();
 
   int x = 0;
   int y = radius;
@@ -298,7 +306,7 @@ List<Point> getEllipsePoints(Offset start, Offset end) {
   int a = (end.dx - start.dx).abs().toInt();
   int b = (end.dy - start.dy).abs().toInt();
   int radius = sqrt(b * b * pow(end.dx - start.dx, 2) +
-          a * a * pow(end.dy - start.dy, 2))
+      a * a * pow(end.dy - start.dy, 2))
       .toInt();
 
   int x = 0;
@@ -423,3 +431,158 @@ List<Point> getParabolaPoints(Offset start, Offset end) {
   return points;
 }
 
+List<Point> getErmitPoints(Offset start, Offset end, Offset startSupport, Offset endSupport) {
+  // 2P(1x) - 2 P(4x) + R(1x) + R(4x) | 2P(1y) - 2 P(4y) + R(1y) + R(4y)
+  // -3P(1x) + 3P(4x) -2R(1x) - R(4x) | -3P(1y) + 3 P(4y) - 2R(1y) - R(4y)
+  // R(1x) | R(1y)
+  // P(1x) | P(1y)
+
+  // x(t) =
+  //  t^3(2P(1x) - 2 P(4x) + R(1x) + R(4x))
+  //  + t^2(-3P(1x) + 3P(4x) -2R(1x) - R(4x))
+  //  + t*R(1x)
+  //  + P(1x)
+
+  // y(t) =
+  // t^3(2P(1y) - 2 P(4y) + R(1y) + R(4y))
+  // + t^2(-3P(1y) + 3 P(4y) - 2R(1y) - R(4y)
+  // + t*R(1y)
+  // + P(1y)
+
+  List<Point> points = [];
+
+  int startX = start.dx.toInt();
+  int startY = start.dy.toInt();
+  int endX = end.dx.toInt();
+  int endY = end.dy.toInt();
+
+  int startSX = startSupport.dx.toInt();
+  int startSY = startSupport.dy.toInt();
+
+  int endSX = endSupport.dx.toInt();
+  int endSY = endSupport.dy.toInt();
+
+  int previousX = -1;
+  int previousY = -1;
+
+  int startT = 0;
+  int endT = 10;
+  for (int i = startT; i <= endT; i ++) {
+    double t = i / (endT - startT).abs();
+
+    int x = (t * t * t * (2 * startX - 2 * endX + startSX + endSX) +
+            t * t * (-3 * startX + 3 * endX - 2 * startSX - endSX) +
+            t * startSX +
+            startX).toInt();
+    int y = (t * t * t * (2 * startY - 2 * endY + startSY + endSY) +
+        t * t * (-3 * startY + 3 * endY - 2 * startSY - endSY) +
+        t * startSY +
+        startY).toInt();
+
+    if (i > 0) {
+      points.addAll(getLineDDAPoints(
+          Offset(previousX.toDouble(), previousY.toDouble()),
+          Offset(x.toDouble(), y.toDouble())));
+    }
+    previousX = x;
+    previousY = y;
+  }
+
+  return points;
+}
+
+List<Point> getBezierPoints(Offset start, Offset end, Offset startSupport, Offset endSupport) {
+  List<Point> points = [];
+
+  int startX = start.dx.toInt();
+  int startY = start.dy.toInt();
+  int endX = end.dx.toInt();
+  int endY = end.dy.toInt();
+
+  int startSX = startSupport.dx.toInt();
+  int startSY = startSupport.dy.toInt();
+
+  int endSX = endSupport.dx.toInt();
+  int endSY = endSupport.dy.toInt();
+
+  int previousX = -1;
+  int previousY = -1;
+
+  int startT = 0;
+  int endT = 10;
+  for (int i = startT; i <= endT; i ++) {
+    double t = i / (endT - startT).abs();
+
+    // -1*P(1x) + 3 * P(2x) -3 P(3x) + P(4x)
+    // 3P(1x) - 6P(2x) + 3P(3x)
+    // -3P(1x) + 3P(3x)
+    // P(1x)
+
+    int x = (t * t * t * (-1 * startX + 3 * startSX - 3 * endSX + endX) +
+            t * t * (3 * startX - 6 * startSX + 3 * endSX) +
+            t * (-3 * startX + 3 * startSX) +
+            startX).toInt();
+    int y = (t * t * t * (-1 * startY + 3 * startSY - 3 * endSY + endY) +
+        t * t * (3 * startY - 6 * startSY + 3 * endSY) +
+        t * (-3 * startY + 3 * startSY) +
+        startY).toInt();
+
+    if (i > 0) {
+      points.addAll(getLineDDAPoints(
+          Offset(previousX.toDouble(), previousY.toDouble()),
+          Offset(x.toDouble(), y.toDouble())));
+    }
+    previousX = x;
+    previousY = y;
+  }
+
+  return points;
+}
+
+List<Point> getBSplinePoints(Offset start, Offset end, Offset startSupport, Offset endSupport) {
+  List<Point> points = [];
+
+  int startX = start.dx.toInt();
+  int startY = start.dy.toInt();
+  int endX = end.dx.toInt();
+  int endY = end.dy.toInt();
+
+  int startSX = startSupport.dx.toInt();
+  int startSY = startSupport.dy.toInt();
+
+  int endSX = endSupport.dx.toInt();
+  int endSY = endSupport.dy.toInt();
+
+  int previousX = -1;
+  int previousY = -1;
+
+  int startT = 0;
+  int endT = 10;
+  for (int i = startT; i <= endT; i ++) {
+    double t = i / (endT - startT).abs();
+
+    // -1 P(1x) + 3 P(2x) -3 P(3x) + R(4x)
+    // 3 P(1x) -6 P(2x) + 3 P(3x)
+    // -3 P(1x) + 3 P(3x)
+    // P(1x) + 4 P(2x) + P(3x)
+
+    int x = (t * t * t * (-1 * startX + 3 * endX - 3 * startSX + endSX) +
+        t * t * (3 * startX - 6 * endX + 3 * startSX) +
+        t * (-3 * startX + 3 * startSX) +
+        startX + 4 * endX + startSX) ~/ 6;
+    int y = (t * t * t * (-1 * startY + 3 * endY - 3 * startSY + endSY) +
+        t * t * (3 * startY - 6 * endY + 3 * startSY) +
+        t * (-3 * startY + 3 * startSY) +
+        startY + 4 * endY + startSY) ~/ 6;
+
+    if (i > 0) {
+      points.addAll(getLineDDAPoints(
+          Offset(previousX.toDouble(), previousY.toDouble()),
+          Offset(x.toDouble(), y.toDouble())));
+    }
+    previousX = x;
+    previousY = y;
+  }
+
+  return points;
+}
